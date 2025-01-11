6bf02d16040fbf107f04f6537380ea50f65da43e
kudchikarsk <kudchikarsk@gmail.com>
2025-01-11 14:51:20 +0530

    Add SendEmailOrderCreatedEventHandler example

---
+++ src/BytLabs.MicroserviceTemplate.Application/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace BytLabs.MicroserviceTemplate.Application.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string email, string title, string body);
+    }
+}
